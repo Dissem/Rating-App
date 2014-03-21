0ee2343f5659b2960a71aa74cbc97c7195474580
Christian Meyer <chrigu.meyer@gmail.com>
2014-03-21 10:40:28 +0100

    Improvements to the drawer navigation

--- RatingApp/src/ch/bfh/mobilecomputing/fs2014/ratingapp/MainActivity.java
+++ RatingApp/src/ch/bfh/mobilecomputing/fs2014/ratingapp/MainActivity.java
@@ -1,11 +1,14 @@
 package ch.bfh.mobilecomputing.fs2014.ratingapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,9 +19,12 @@ import ch.bfh.mobilecomputing.fs2014.ratingapp.entities.SurveyRepository;
 public class MainActivity extends FragmentActivity implements
 		ItemListFragment.Callbacks {
 	private DrawerLayout drawerLayout;
+	private ActionBarDrawerToggle drawerToggle;
 	private ListView drawerList;
 	private ItemListAndDetailFragment itemListAndDetailFragment;
 	private SurveyRepository surveyRepo;
+	private CharSequence title;
+	private CharSequence drawerTitle;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +50,55 @@ public class MainActivity extends FragmentActivity implements
 			showEnterCode(false);
 		else
 			showSurvey(false);
+
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+				getActionBar().setTitle(title);
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				getActionBar().setTitle(drawerTitle);
+			}
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		drawerLayout.setDrawerListener(drawerToggle);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	private class DrawerItemClickListener implements
