46f4fe9b1dcc3dff5cfe331bee2f866c4bec721b
Christian Meyer <chrigu.meyer@gmail.com>
2014-03-31 07:26:08 +0200

    Test project - no actual tests yet, feel free to add some

---
+++ RatingAppTest/gen/ch/bfh/mobilecomputing/fs2014/ratingapp/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ch.bfh.mobilecomputing.fs2014.ratingapp.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
